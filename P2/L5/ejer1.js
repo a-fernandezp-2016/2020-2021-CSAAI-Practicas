87f974b654a396e4736d2b74042992edc53da6b0
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-04-08 20:07:02 +0200

    Ejer 1 js definitivo

--- P2/L5/ejer1.js
+++ P2/L5/ejer1.js
@@ -3,21 +3,7 @@ console.log("Esperando a que el botón sea pulsado.......");
 //-- Obtener el párrafo
 const imprime = document.getElementById('Print');
 
-// Variable contador.
-let cont = true;
-
 //-- Funcion de retrollamada de la pulsación del párrafo
 imprime.onclick = () => {
-
-    // Condicionante.
-    if (cont == true)
-    {
-        console.log(" ¡Has entrado en la NAVE DEL MISTERIO! ");
-        let cont = false;
-    }
-    else
-    {
-        console.log(" ¡Has entrado en la NAVE DE LA COMEDIA! ");
-        let cont = true;
-    }
+    console.log(" ¡Has entrado en la NAVE DEL MISTERIO! ");
 }
