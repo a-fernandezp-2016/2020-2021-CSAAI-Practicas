91837725c3c7ea1449b58aa608934225fb8085f2
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-04-08 19:41:01 +0200

    Cambio 2 de js ejer 1

--- P2/L5/ejer1.js
+++ P2/L5/ejer1.js
@@ -3,11 +3,11 @@ console.log("Esperando a que el botón sea pulsado.......");
 //-- Obtener el párrafo
 const imprime = document.getElementById('Print');
 
+// Variable contador.
+var cont = 0;
+
 //-- Funcion de retrollamada de la pulsación del párrafo
 imprime.onclick = () => {
-
-    // Variable contador.
-    var cont = 0;
 
     // Condicionante.
     if (cont == 0)
@@ -18,5 +18,6 @@ imprime.onclick = () => {
     else
     {
         console.log(" ¡Has entrado en la NAVE DE LA COMEDIA! ");
+        var cont = 0;
     }
 }
