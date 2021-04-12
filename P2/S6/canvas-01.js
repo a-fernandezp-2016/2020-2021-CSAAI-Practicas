5806f8e72120ae83a119d1b5922e4597af9864ed
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-04-12 19:35:27 +0200

    Ejemplo 1

---
+++ P2/S6/canvas-01.js
@@ -0,0 +1,9 @@
+// Mensaje de iniciación en la consola de comandos.
+console.log("Ejecutando JS....");
+
+// Leemos el canvas de html en JS y, lo almacenamos en la cte canvas.
+const canvas = document.getElementById("canvas");
+
+// Definimos las dimensiones del canvas.
+canvas.width = 400;
+canvas.height = 250;
