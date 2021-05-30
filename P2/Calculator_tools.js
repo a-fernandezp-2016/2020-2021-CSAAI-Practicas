9d5ad83a1056dcf9c75a89cc32f68af4e380281c
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-05-31 00:46:31 +0200

    New

--- P2/Calculator_tools.js
+++ P2/Calculator_tools.js
@@ -1,37 +1,67 @@
 // Hecho por: Alejandro Fernández Pérez.
 
+// Empezamos con la función start.
 function start() {
   // Mensaje de inicio de ejecución en consola.
   console.log("Página cargada....");
   console.log("Ejecutando Calculadora HUAWEI con seguridad");
-}
+  // Variable numeros a partir de la clase digito, para crear el array de los 10 nº.
+  let numeros = document.getElementsByClassName("digito");
+  // Variable display a partir del ID display.
+  let display = document.getElementById("display");
+  // Variable operandos a partir de la clase operacion, para crear el array de los operandos.
+  let operandos = document.getElementsByClassName("operacion");
+  // Variable igual a partir del ID igual.
+  let igual = document.getElementById("igual");
+  // Variable de la fase inicial.
+  let fase = FASES.INIT;
 
-// Fase en que estas de la calculadora:
-// Fase 0: inicial, no has metido nada.
-// Fase 1: 1º operando.
-// Fase 2: operador.
-// Fase 3: 2º operando.
-// Fase 4: resultado. Este es dandole a igual, no aparece en la estructura de const
-// porque es la misma que la fase 1 en verdad, ya que el resultado es 1 nº solo.
-const FASES = {
+  // Fase en que estas de la calculadora:
+  // Fase 0: inicial, no has metido nada.
+  // Fase 1: 1º operando.
+  // Fase 2: operador.
+  // Fase 3: 2º operando.
+  // Fase 4: resultado. Este es dandole a igual, no aparece en la estructura de const
+  // porque es la misma que la fase 1 en verdad, ya que el resultado es 1 nº solo.
+  const FASES = {
     INIT: 0,
     OP1: 1,
     OPER: 2,
     OP2: 3,
+  }
+
+  // Acciones de la calculadora (pulsando botones).
+
+  // Pulsando 1 de los 10 nº.
+  for (i=0; i<numeros.length; i++) {
+    //-- Establecer la funcion de llamada del boton del 0 al 9.
+    //-- El parámetro ev.target contiene el boton
+    //-- que ha recibido el click, y por el valor "value".
+    numeros[i].onclick = (ev) => {
+      digito(ev.target.value);
+    }
+  }
+  // Pulsando un operando.
+  for (j=0; j<operandos.length; j++) {
+    operandos[j].onclick = (ev) => {
+        if(fase == FASES.OP1)
+        {
+          display.innerHTML += ev.target.value;
+          fase = FASES.OPER;
+          console.log(fase,"Ahora estas en el operando");
+        }
+    }
+  }
+  // Pulsando el botón igual.
+  igual.onclick = (ev) => {
+    if(fase == FASES.OP1 || fase == FASES.OP2)
+    {
+      display.innerHTML = eval(display.innerHTML);
+      fase = FASES.OP1;
+      console.log(fase,"Ahora estas en la FASE DE RESULTADO 4");
+    }
+  }
 }
-
-// Asignar la clase de los números a una variable o array de numeros.
-let numeros = document.getElementsByClassName("digito")
-// Asignar el id del display a la variable del display.
-let display = document.getElementById("display")
-// Asignar la clase de los operandos a una variable o array operandos.
-let operandos = document.getElementsByClassName("operacion")
-// Asignar el id de igual a la variable del igual.
-let igual = document.getElementById("igual")
-
-// Variable de la fase inicial.
-let fase = FASES.INIT;
-
 //-- Función de retrollamada de los botones
 //-- de la función digito donde se procesan las operaciones.
 function digito(boton)
@@ -52,37 +82,3 @@ function digito(boton)
       }
     }
 }
-// Realización de la expresión igual.
-igual.onclick = (ev) => {
-  if(fase == FASES.OP1 || fase == FASES.OP2)
-  {
-    display.innerHTML = eval(display.innerHTML);
-    fase = FASES.OP1;
-    console.log(fase,"Ahora estas en la FASE DE RESULTADO 4");
-  }
-}
-
-// Bucle que recorre todos los operandos y pasa al siguiente estado
-// del OP1 al OPER.
-for (j=0; j<operandos.length; j++) {
-  operandos[j].onclick = (ev) => {
-      if(fase == FASES.OP1)
-      {
-        display.innerHTML += ev.target.value;
-        fase = FASES.OPER;
-        console.log(fase,"Ahora estas en el operando");
-      }
-  }
-}
-
-// Bucle que recorre todos los nº del 0 al 9 y saca como valor a la funcion digito
-// al que se le haya hecho click.
-for (i=0; i<numeros.length; i++) {
-
-    //-- Establecer la funcion de llamada del boton del 0 al 9.
-    //-- El parámetro ev.target contiene el boton
-    //-- que ha recibido el click, y por el valor "value".
-    numeros[i].onclick = (ev) => {
-      digito(ev.target.value);
-    }
-  }
