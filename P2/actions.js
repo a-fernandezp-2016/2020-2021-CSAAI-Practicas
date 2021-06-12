9971c903b6679bf9b85aed155765747a5d95f469
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-06-12 14:18:52 +0200

    P2

--- P2/actions.js
+++ P2/actions.js
@@ -1,7 +1,10 @@
 // Hecho por: Alejandro Fernández Pérez.
 
-// Punto de inicio en el depurador del navegador.
-console.log("Arranca la Calculadora HUawei Web.....");
+// Punto de inicio en el consola del navegador.
+console.log("Arranca la Calculadora Huawei Web.....");
+console.log("Esperando a utilizarse....");
+// Mensaje-recordatorio de donde empiezas.
+console.log("Al ejecutar la pag HTML de la calculadora Huawei Web, empiezas en la fase 0: inicial");
 
 // Crear un elemento display a partir del ID display.
 display = document.getElementById("display")
@@ -9,6 +12,14 @@ display = document.getElementById("display")
 operadores = document.getElementsByClassName("operador");
 //-- Obtener el array con todos los elementos de la clase digito, que son 10 nº.
 numeros = document.getElementsByClassName("digito");
+// Crear un elemento igual, para dar la solucion a la operación mostrada en display, a partir del ID igual.
+igual = document.getElementById("igual");
+// Crear un elemento para reiniciar el display o pantalla a 0, mostrando el 0,
+// a partir del ID clear.
+clear = document.getElementById("clear");
+// Crear un elemento para borrar el último nº o carácter del display o pantalla,
+// empezando por la derecha, a partir del ID del.
+del = document.getElementById("del");
 
 // Momento/Fase en que estas de la calculadora:
 // Momento/Fase 0: inicial, no has metido nada.
@@ -28,8 +39,8 @@ const MOMENTO = {
 // Variable de la fase inicial.
 let fase = MOMENTO.INIT;
 
-//-- Función de retrollamada de los botones
-//-- de la función digito donde se procesan las operaciones.
+//-- La función digito es una función donde se procesan las operaciones con los
+// dígitos, en cada momento o cada fase.
 function digito(ev)
 {
     // Empezamos comprobando, con el estado o momento inicial.
@@ -40,7 +51,7 @@ function digito(ev)
       // Pasamos al siguiente estado.
       fase = MOMENTO.OP1;
       // Ponemos un mensaje en consola, para avisar.
-      console.log(fase,"Ahora estas en el operador 1");
+      console.log(fase,"Ahora estas en la fase 1: operando 1.");
     }
     else if(fase == MOMENTO.OP1 || fase == MOMENTO.OP2 || fase == MOMENTO.OPER)
     {
@@ -51,24 +62,29 @@ function digito(ev)
       {
         // Pasamos al siguiente estado.
         fase = MOMENTO.OP2;
-        // Ponemos un mensaje en consola, para avisar.
-        console.log(fase,"Ahora estas en el operando 2.");
+        // Ponemos un mensaje en consola del navegador, para avisar.
+        console.log(fase,"Ahora estas en la fase 3: operando 2.");
       }
-      else
+      else if(fase == MOMENTO.OP1)
       {
-        // Ponemos un mensaje en consola, para avisar.
-        console.log(fase,"Sigues en la fase 1.");
+        // Ponemos un mensaje en consola del navegador, para avisar.
+        console.log(fase,"Sigues en la fase 1: operando 1.");
+      }
+      else if(fase == MOMENTO.OP2)
+      {
+        // Ponemos un mensaje en consola del navegador, para avisar.
+        console.log(fase,"Sigues en la fase 3: operando 2.");
       }
     }
 }
 
-// Para pulsar uno de los 10 digitos o nº que componen la calculadora web.
+// Al pulsar un dígito de los 10 digitos o nº que componen la calculadora Huawei web.
 for (let boton of numeros) {
-  //-- Al pulsar un dígito, se accede a la function digito.
+  // Al pulsar, accedemos a la función digito.
   boton.onclick = digito;
 }
 
-// Al pulsar un operador.
+// Al pulsar un botón de los operadores.
 for (let operador of operadores) {
   operador.onclick = (ev) => {
       if(fase == MOMENTO.OP1)
@@ -77,13 +93,51 @@ for (let operador of operadores) {
         display.innerHTML += ev.target.value;
         // Pasamos al siguiente estado.
         fase = MOMENTO.OPER;
-        // Ponemos un mensaje en consola, para avisar.
-        console.log(fase,"Ahora estas en el operador");
+        // Ponemos un mensaje en consola del navegador, para avisar.
+        console.log(fase,"Ahora estas en la fase 2: operador");
       }
       else
       {
-        // Ponemos un mensaje en consola, para avisar.
-        console.log(fase,"Todavía no es momento para pulsar a un operador.");
+        // Ponemos un mensaje en consola del navegador, para avisar.
+        console.log(fase,"PROCESO NO VÁLIDO, LO SIENTO");
       }
   }
+}
+
+// Al pulsar el botón igual, nos da el resultado final de la operación.
+igual.onclick = () => {
+  if(fase == MOMENTO.OP1 || fase == MOMENTO.OP2)
+  {
+    // Evaluamos lo que hay en pantalla o display con función eval(). 
+    // Y sobreescribimos lo que hay en dicha pantalla o display 
+    // con el resultado final de la operación evaluada.
+    display.innerHTML = eval(display.innerHTML);
+    // Pasamos a la fase 1.
+    fase = MOMENTO.OP1;
+    // Ponemos un mensaje en consola del navegador, para avisar.
+    console.log(fase,"Ahora estas en la fase 1: operando 1");
+  }
+  else
+  {
+    // Ponemos un mensaje en consola del navegador, para avisar.
+    console.log(fase,"PROCESO NO VÁLIDO, LO SIENTO");
+  }
+}
+
+// Al pulsar el botón de AC, para reiniciar a 0, mostrando el 0.
+clear.onclick = (ev) => {
+  // Reiniciar el display o pantalla a 0, mostrando el 0.
+  display.innerHTML = ev.target.value;
+  // Pasamos a la fase 0.
+  fase = MOMENTO.INIT;
+  // Ponemos un mensaje en consola del navegador, para avisar.
+  console.log(fase,"Ahora estas en la fase 0: inicial");
+}
+
+// Al pulsar el botón de DEL, borras el último nº o carácter del display,
+// empezando por la derecha.
+del.onclick = (ev) => {
+  // Borrar el último nº o carácter del display, empezando por la derecha.
+  display.innerHTML = display.innerHTML.slice(0,-1);
+  // Procedimiento de cambio de fase o momento.
 }
