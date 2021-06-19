ccc8a022a5bebb6a4512e3ae597fee17ac8bc79b
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-06-19 14:05:27 +0200

    P3

--- P3/actions.js
+++ P3/actions.js
@@ -8,20 +8,16 @@ console.log("Empezamos en la fase inicial 0...");
 const pantalla = document.getElementById("canvas");
 
 // Definimos el tamaño de la pantalla sobre la que jugar o el canvas.
-pantalla.width = 550;
-pantalla.height = 800;
+pantalla.width = 600;
+pantalla.height = 1000;
 
 // Creamos el elemento ID del botón play para poder pulsarlo.
 const play = document.getElementById("play");
 // Creamos el elemento ID del botón restart para poder pulsarlo.
 const restart = document.getElementById("restart");
 
-// Definimos el texto sólido de puntuación, tiempo y vidas.
-const texto = pantalla.getContext("2d");
-// Definimos la raqueta, en forma de rectángulo.
-const raqueta = pantalla.getContext("2d");
-// Definimos el objeto bola, en forma de círculo.
-const bola = pantalla.getContext("2d");
+// Definimos el contenido de la pantalla o canvas para poder dibujar en ello.
+const paintIT = pantalla.getContext("2d");
 
 // Diagrama de estados. Hay 4 estados: 0, 1, 2 y 3.
 // El Estado 0 es el inicial, que cambia al Estado 1 cuando pulsamos espacio.
@@ -38,119 +34,128 @@ const ESTADO = {
     FINAL: 3
 }
 
-// Dimensiones raqueta.
-let anchoRAQ = 70;
-let altoRAQ = 12;
+// Características de los textos sólidos de arriba de la pantalla.
+    // Coordenadas de la puntuación.
+    let puntX = 50;
+    let puntY = 60;
+    // Variable de la puntuación total.
+    let score = 0;
+    // Coordenadas de las vidas.
+    let vidX = 380;
+    let vidY = 60;
+    // Variable de las vidas totales.
+    let vidas = 3;
+    // Coordenadas del tiempo.
+    let timeX = 50;
+    let timeY = 120;
+    // Variable tiempo.
+    // let timer = ;
+
+// Características de la línea de separación trazada para separar la cabecera del juego en sí.
+    // Coordenadas de la separación de la cabecera con el juego en sí.
+    let separX = 0;
+    let separY = 165;
+
+// Características de la raqueta.
+    // Dimensiones raqueta.
+    let anchoRAQ = 100;
+    let altoRAQ = 25;
+    // Definimos las coordenadas de la raqueta.
+    let raqX = 275;
+    let raqY = 650;
+// Definimos las coordenadas de la bola.
+let bolaX = 274;
+let bolaY = 650;
 
 // Definimos variable velocidad del eje x.
 let velX = 5;
-// Definimos variable posición del eje x.
-let posX = 0;
 // Definimos la variable fase, para ir cambiando de fase y saber donde estamos.
 let fase = ESTADO.INIT;
 
-// Función mensaje bienvenida.
-function InitialMessag() {
-    pantalla.innerHTML = "¡QUÉ EMPIECE EL JUEGO!";
-}
+// Definimos la vidas que tiene un jugador.
+let vidas = 3;
 
 // Estructuramos el texto sólido.
-// De puntuación.
-texto.font = "40px Arial";
-texto.fillStyle = 'white';
-texto.fillText("Puntuación: ",32,80);
-//texto.fillText(score, 35,80);
-// De vidas.
-texto.font = "40px Arial";
-texto.fillStyle = 'white';
-texto.fillText("Vidas: ",350,80);
-//texto.fillText(vidas, 38,80);
-// De tiempo.
-texto.font = "35px Arial";
-texto.fillStyle = 'white';
-texto.fillText("Tiempo: ",102,130);
+    // De puntuación.
+    paintIT.font = "35px Arial";
+    paintIT.fillStyle = 'white';
+    paintIT.fillText("Puntuación: ",puntX,puntY);
+    //texto.fillText(score, 35,80);
+    // De vidas.
+    paintIT.font = "35px Arial";
+    paintIT.fillStyle = 'white';
+    paintIT.fillText("Vidas: ",vidX,vidY);
+    //texto.fillText(vidas, 38,80);
+    // De tiempo.
+    paintIT.font = "35px Arial";
+    paintIT.fillStyle = 'white';
+    paintIT.fillText("Tiempo: ",timeX,timeY);
 
-// Delimitación del objeto bola.
-bola.beginPath();
-    // Creamos el círculo con la función arc(posición x, posición y, radio, ángulo inicial, ángulo final);
-    bola.arc(225,430,10,0,2*Math.PI);
-    // Rellenamos el rectángulo de blanco.
-    bola.fillStyle = 'white';
-    // Mostramos el trazo.
-    bola.stroke();
-    // Mostramos el relleno.
-    bola.fill();
-bola.closePath();
-
-// Animación de la raqueta a través de la función update.
-function update() 
+// Trazamos la línea de separación: cabecera de textos - juego en sí,
+// a través del bucle for. Usando líneas discontinuas.
+for(let i=separX; i<600; i += 100)
 {
-  //-- Implementación del algoritmo de animación de la raqueta:
-  console.log("Analizando posición de la raqueta....");
-  //-- 1) Actualizar posicion de los elementos.
-  posX = posX + velX;
-  //-- 2) Borrar el canvas.
-  raqueta.clearRect(0, 0, pantalla.width, pantalla.height);
-  //-- 3) Pintar los elementos en el canvas, delimitando el objeto raqueta.
-  raqueta.beginPath();
-        // Creamos el rectángulo con la función rect(posición x variable, posición y fija, ancho, alto);
-        raqueta.rect(posX, 630, anchoRAQ, altoRAQ);
-        //-- Rellenamos el rectángulo de blanco.
-        raqueta.fillStyle = 'white';
-        // Mostramos el trazo.
-        raqueta.stroke();
-        // Mostramos el relleno.
-        raqueta.fill();
-  raqueta.closePath();
-  //-- 4) Repetir
-  requestAnimationFrame(update);
-}
-
-// Para empezar la partida, hay que pulsar el botón play.
-play.onclick = () => {
-    if(fase == ESTADO.INIT)
+    for(let j=60; j<600; j += 100)
     {
-        timeinit = setInterval(InitialMessag, 2000)
-        clearInterval(timeinit)
-        // Cambiamos a la fase 1 o de saque.
-        fase = ESTADO.SAQUE;
-    }
-    else
-    {
-        // Mensaje de error o proceso inválido.
-        console.log("PROCESO NO VÁLIDO: SÓLO ES PARA INICIAR EL JUEGO...");
+        // Inicio trazo.
+        paintIT.beginPath();
+        //-- Trazo de la línea horizontal desde el pto inicial al final.
+        paintIT.moveTo(i,separY);
+        paintIT.lineTo(j, separY);
+        // Coloreamos de blanco la línea.
+        paintIT.strokeStyle = 'white';
+        //-- Le ponemos un tamaño visible al trazo.
+        paintIT.lineWidth = 4;
+        //-- Mostrar el trazo.
+        paintIT.stroke()
+        // Final trazo.
+        paintIT.closePath()
+        // Parte o trazo invisible.
+        paintIT.beginPath();
+        //-- Trazo de la línea horizontal desde el pto inicial al final.
+        paintIT.moveTo(j,separY);
+        paintIT.lineTo(j+=40, separY);
+        // Coloreamos de blanco la línea.
+        paintIT.strokeStyle = 'black';
+        //-- Mostrar el trazo.
+        paintIT.stroke()
+        // Final trazo.
+        paintIT.closePath()
     }
 }
 
-// Para reiniciar la partida, hay que pulsar el botón restart.
-restart.onclick = () => {
-    // Volvemos a la fase 0 o inicial.
-    fase = ESTADO.INIT;
-}
+// // Delimitación del objeto bola.
+// paintIT.beginPath();
+//     // Creamos el círculo con la función arc(posición x, posición y, radio, ángulo inicial, ángulo final);
+//     paintIT.arc(225,430,10,0,2*Math.PI);
+//     // Rellenamos el rectángulo de blanco.
+//     paintIT.fillStyle = 'white';
+//     // Mostramos el trazo.
+//     paintIT.stroke();
+//     // Mostramos el relleno.
+//     paintIT.fill();
+// paintIT.closePath()
 
-// Al pulsar 1 de las 3 teclas del teclado QWERTY: ESPACIO / FLECHA DER / FLECHA IZQ.
-window.onkeydown = (e) => {
-
-    if((fase != ESTADO.INIT) && ((e.key == ' ') || (e.keyCode == '39') || (e.keyCode == '37')))
-    {
-        if(e.key == ' ')
-        {
-
-        }
-        // Si pulsamos la flecha de la derecha.
-        else if(e.keyCode == '39')
-        {
-
-        }
-        // Si pulsamos la flecha de la izquierda.
-        else if(e.keyCode == '37')
-        {
-
-        }
-    }
-    else
-    {
-        // Mensaje de error o proceso inválido.
-        console.log("PROCESO NO VÁLIDO: Debes estar en la fase inicial y haber dado espacio o flecha der/izq.");
-    }
-}
+// // Animación de la raqueta y de la bola a través de la función update.
+// function update() 
+// {
+//   //-- Implementación del algoritmo de animación de la raqueta:
+//   console.log("Analizando posición de la raqueta y de la bola....");
+//   //-- 1) Actualizar posicion de los elementos.
+//   raqX = raqX + velX;
+//   //-- 2) Borrar el canvas.
+//   paintIT.clearRect(0, 0, pantalla.width, pantalla.height);
+//   //-- 3) Pintar los elementos en el canvas, delimitando el objeto raqueta.
+//   paintIT.beginPath();
+//         // Creamos el rectángulo con la función rect(posición x variable, posición y fija, ancho, alto);
+//         paintIT.rect(raqX, raqY, anchoRAQ, altoRAQ);
+//         //-- Rellenamos el rectángulo de blanco.
+//         paintIT.fillStyle = 'white';
+//         // Mostramos el trazo.
+//         paintIT.stroke();
+//         // Mostramos el relleno.
+//         paintIT.fill();
+//   paintIT.closePath()
+//   //-- 4) Repetir
+//   requestAnimationFrame(update);
+// }
