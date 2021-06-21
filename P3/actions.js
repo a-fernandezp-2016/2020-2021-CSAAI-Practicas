3dc98d7f94fbaec773d11281f3d28272efac6ccc
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-06-21 19:44:44 +0200

    P3 js pinceladas

--- P3/actions.js
+++ P3/actions.js
@@ -289,21 +289,22 @@ function update()
             velY_bol = -velY_bol;
         }
         // Condición para que rebote la bola en la raqueta.
-        if(((bolaX-radio) >= raqX) && ((bolaX-radio) <= (raqX + anchoRAQ)) && ((bolaY-radio) >= raqY) && ((bolaY-radio) <= (raqY + altoRAQ)))
+        if((((bolaX-radio) <= raqX) || ((bolaX-radio) >= (raqX + anchoRAQ))) && 
+        (((bolaY-radio) <= raqY) || ((bolaY-radio) >= (raqY + altoRAQ))))
         {
             // Cálculo del rebote en eje x e y.
             velX_bol = -velX_bol;
             velY_bol = -velY_bol;
         }
         // Condición para que la bola no circule más abajo de por donde se mueve la raqueta.
-        else if((bolaY-radio) < (raqY + altoRAQ))
+        else if((bolaY-radio) <= raqY)
         {
             // Movimiento de la bola en el eje x.
             bolaX += velX_bol;
             // Movimiento de la bola en el eje y.
             bolaY += velY_bol;
         }
-        else
+        else if((bolaY-radio) >= (raqY + altoRAQ))
         {
             if(vidas > 0)
             {
