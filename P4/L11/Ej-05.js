49692489b255cca332984c1cc673fd094cac56fc
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-06-30 21:26:21 +0200

    Ejemplo 5 P4

---
+++ P4/L11/Ej-05.js
@@ -0,0 +1,63 @@
+console.log("Ejecutando JS....")
+
+//-- Obtener elementos del DOM
+const canvas = document.getElementById('canvas');
+const img = document.getElementById('imagesrc');
+const ctx = canvas.getContext('2d');
+
+// Obtener el elemento del deslizador para interconectarlo con la pag HTML.
+const deslizador = document.getElementById('deslizador');
+
+// Obtener el elemento del display del deslizador para interconectarlo con la pag HTML.
+const display = document.getElementById('range_value');
+
+//-- Función de retrollamada de imagen cargada
+//-- La imagen no se carga instantaneamente, sino que
+//-- lleva un tiempo. Sólo podemos acceder a ella una vez
+//-- que esté totalmente cargada
+img.onload = function () 
+{
+
+    //-- Se establece como tamaño del canvas el mismo
+    //-- que el de la imagen original
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img,0,0);
+
+    // Mensaje indicando que la imagen ya está puesta en el canvas.
+    console.log("Imagen lista...");
+};
+
+//-- Funcion de retrollamada del deslizador
+deslizador.oninput = () => 
+{
+    //-- Mostrar el nuevo valor del deslizador
+    display.innerHTML = deslizador.value;
+
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img, 0,0);
+
+    //-- Obtener la imagen del canvas en pixeles
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    //-- Obtener el array con todos los píxeles
+    let data = imgData.data
+
+    //-- Obtener el umbral de rojo del desliador
+    umbral = deslizador.value
+
+    //-- Filtrar la imagen según el nuevo umbral
+    for (let i = 0; i < data.length; i+=4) 
+    {
+        if (data[i] > umbral)
+        data[i] = umbral;
+    }
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+};
+
+console.log("Fin...");
