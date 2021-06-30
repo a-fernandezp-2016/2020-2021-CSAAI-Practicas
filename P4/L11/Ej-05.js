22ce864a885191c9ebde4ff71f88c85c7ca84cdf
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-06-30 23:25:43 +0200

    Renew

--- P4/L11/Ej-05.js
+++ P4/L11/Ej-05.js
@@ -12,6 +12,9 @@ const deslizadorG = document.getElementById("deslizadorG");
 // Obtener el elemento del deslizador del canal B para interconectarlo con la pag HTML.
 const deslizadorB = document.getElementById("deslizadorB");
 
+// Crear el elemento del botón para cambiar a escala de grises.
+const escalaGrises = document.getElementById("scale_gray");
+
 // Obtener el elemento del display del deslizador del canal R para interconectarlo con la pag HTML.
 const displayR = document.getElementById('displayR');
 // Obtener el elemento del display del deslizador del canal G para interconectarlo con la pag HTML.
@@ -39,13 +42,71 @@ img.onload = function ()
     console.log("Imagen lista...");
 };
 
-//-- Funcion de retrollamada del deslizador
-deslizador.oninput = () => 
+//-- Funcion de retrollamada del deslizador del canal R.
+deslizadorR.oninput = () => 
 {
     //-- Mostrar el nuevo valor del deslizador R en su display.
     displayR.innerHTML = deslizadorR.value;
+
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img, 0,0);
+
+    //-- Obtener la imagen del canvas en pixeles
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    //-- Obtener el array con todos los píxeles
+    let dataR = imgData.data
+
+    //-- Obtener el umbral de rojo del deslizador.
+    umbralR = deslizadorR.value
+
+    //-- Filtrar la imagen según el nuevo umbral de R.
+    for (let i = 0; i < dataR.length; i+=4) 
+    {
+        if(dataR[i] > umbralR)
+        {
+            dataR[i] = umbralR;
+        }
+    }
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+};
+
+//-- Funcion de retrollamada del deslizador del canal G.
+deslizadorG.oninput = () => 
+{
     //-- Mostrar el nuevo valor del deslizador G en su display.
     displayG.innerHTML = deslizadorG.value;
+
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img, 0,0);
+
+    //-- Obtener la imagen del canvas en pixeles
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    //-- Obtener el array con todos los píxeles
+    let dataG = imgData.data
+
+    //-- Obtener el umbral de verde del deslizador.
+    umbralG = deslizadorG.value
+
+    //-- Filtrar la imagen según el nuevo umbral de G.
+    for(let i = 1; i < dataG.length; i+=4)
+    {
+        if(dataG[i] > umbralG)
+        {
+            dataG[i] = umbralG;
+        }
+    }
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+};
+
+//-- Funcion de retrollamada del deslizador del canal B.
+deslizadorB.oninput = () => 
+{
     //-- Mostrar el nuevo valor del deslizador B en su display.
     displayB.innerHTML = deslizadorB.value;
 
@@ -57,43 +118,26 @@ deslizador.oninput = () =>
     let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
     //-- Obtener el array con todos los píxeles
-    let data = imgData.data
+    let dataB = imgData.data
 
-    //-- Obtener el umbral de rojo del deslizador.
-    umbralR = deslizadorR.value
-    //-- Obtener el umbral de verde del deslizador.
-    umbralG = deslizadorG.value
     //-- Obtener el umbral de azul del deslizador.
     umbralB = deslizadorB.value
 
-    //-- Filtrar la imagen según los nuevos umbrales.
-    for (let i = 0; i < data.length; i++) 
+    //-- Filtrar la imagen según el nuevo umbral de B.
+    for(let i = 2; i < dataB.length; i+=4)
     {
-        brillo = (3 * umbralR + 4 * umbralG + umbralB)/8;
-        if(data[i] == data[4*i])
+        if(dataB[i] > umbralB)
         {
-            if(data[i] > umbralR)
-            {
-                data[i] = umbralR;
-            }
-        }
-        else if(data[i] == data[4*i+1])
-        {
-            if(data[i] > umbralG)
-            {
-                data[i] = umbralG;
-            }
-        }
-        else if(data[i] == data[4*i+2])
-        {
-            if(data[i] > umbralB)
-            {
-                data[i] = umbralB;
-            }
+            dataB[i] = umbralB;
         }
     }
     //-- Poner la imagen modificada en el canvas
     ctx.putImageData(imgData, 0, 0);
 };
+
+escalaGrises.onclick = () => 
+{
+    brillo = (3 * umbralR + 4 * umbralG + umbralB)/8;
+}
 
 console.log("Fin...");
