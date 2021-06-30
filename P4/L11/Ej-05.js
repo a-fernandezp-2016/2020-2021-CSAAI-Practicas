6565bac4a0b62414e0b8d28bca822213564c9fb0
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-06-30 23:03:07 +0200

    Ejemplo 5 version 2

--- P4/L11/Ej-05.js
+++ P4/L11/Ej-05.js
@@ -5,11 +5,19 @@ const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
 
-// Obtener el elemento del deslizador para interconectarlo con la pag HTML.
-const deslizador = document.getElementById('deslizador');
+// Obtener el elemento del deslizador del canal R para interconectarlo con la pag HTML.
+const deslizadorR = document.getElementById('deslizadorR');
+// Obtener el elemento del deslizador del canal G para interconectarlo con la pag HTML.
+const deslizadorG = document.getElementById("deslizadorG");
+// Obtener el elemento del deslizador del canal B para interconectarlo con la pag HTML.
+const deslizadorB = document.getElementById("deslizadorB");
 
-// Obtener el elemento del display del deslizador para interconectarlo con la pag HTML.
-const display = document.getElementById('range_value');
+// Obtener el elemento del display del deslizador del canal R para interconectarlo con la pag HTML.
+const displayR = document.getElementById('displayR');
+// Obtener el elemento del display del deslizador del canal G para interconectarlo con la pag HTML.
+const displayG = document.getElementById('displayG');
+// Obtener el elemento del display del deslizador del canal B para interconectarlo con la pag HTML.
+const displayB = document.getElementById('displayB');
 
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
@@ -34,8 +42,12 @@ img.onload = function ()
 //-- Funcion de retrollamada del deslizador
 deslizador.oninput = () => 
 {
-    //-- Mostrar el nuevo valor del deslizador
-    display.innerHTML = deslizador.value;
+    //-- Mostrar el nuevo valor del deslizador R en su display.
+    displayR.innerHTML = deslizadorR.value;
+    //-- Mostrar el nuevo valor del deslizador G en su display.
+    displayG.innerHTML = deslizadorG.value;
+    //-- Mostrar el nuevo valor del deslizador B en su display.
+    displayB.innerHTML = deslizadorB.value;
 
     //-- Situar la imagen original en el canvas
     //-- No se han hecho manipulaciones todavia
@@ -47,14 +59,38 @@ deslizador.oninput = () =>
     //-- Obtener el array con todos los píxeles
     let data = imgData.data
 
-    //-- Obtener el umbral de rojo del desliador
-    umbral = deslizador.value
+    //-- Obtener el umbral de rojo del deslizador.
+    umbralR = deslizadorR.value
+    //-- Obtener el umbral de verde del deslizador.
+    umbralG = deslizadorG.value
+    //-- Obtener el umbral de azul del deslizador.
+    umbralB = deslizadorB.value
 
-    //-- Filtrar la imagen según el nuevo umbral
-    for (let i = 0; i < data.length; i+=4) 
+    //-- Filtrar la imagen según los nuevos umbrales.
+    for (let i = 0; i < data.length; i++) 
     {
-        if (data[i] > umbral)
-        data[i] = umbral;
+        brillo = (3 * umbralR + 4 * umbralG + umbralB)/8;
+        if(data[i] == data[4*i])
+        {
+            if(data[i] > umbralR)
+            {
+                data[i] = umbralR;
+            }
+        }
+        else if(data[i] == data[4*i+1])
+        {
+            if(data[i] > umbralG)
+            {
+                data[i] = umbralG;
+            }
+        }
+        else if(data[i] == data[4*i+2])
+        {
+            if(data[i] > umbralB)
+            {
+                data[i] = umbralB;
+            }
+        }
     }
     //-- Poner la imagen modificada en el canvas
     ctx.putImageData(imgData, 0, 0);
