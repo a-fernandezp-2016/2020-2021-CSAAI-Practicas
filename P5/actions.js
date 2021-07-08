c1c92a3735baa81b703bc6552f93be4a67c36880
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-07-08 20:40:45 +0200

    html y js 4

--- P5/actions.js
+++ P5/actions.js
@@ -26,13 +26,6 @@ const ImgStatic = "Imagen_static_FFTV.png";
 // Establecer las dimensiones del vídeo en directo.
 vidPrincipal.width = 640;
 vidPrincipal.height = 360;
-// Establecer las dimensiones de los vídeos a elegir.
-VidPadel.width = 426;
-VidPadel.height = 240;
-VidTenis.width = 426;
-VidTenis.height = 240;
-VidFutbol.width = 426;
-VidFutbol.height = 240;
 
 // Fase de estados para indicar si partimos desde el inicio con cámara OFF o pasamos
 // al siguiente estado con cámara en ON.
@@ -61,9 +54,9 @@ botonCamON.onclick = () =>
         // Se establece por defecto la imagen estática en la emisión del vídeo en directo.
         vidPrincipal.poster = ImgStatic;
         // Se establecen en los 3 vídeos a elegir, sus propios vídeos, sacados de mi Google Drive (son públicos).
-        VidPadel.src="https://drive.google.com/uc?export=download&id=1RNi4ng6pxVvLT9-ZSbQvPE0lZO-qXp8V";
-        VidTenis.src="https://drive.google.com/uc?export=download&id=1SY0V5l7IQyKuOY03kn8CAn-nPBGY6liK";
-        VidFutbol.src="https://drive.google.com/uc?export=download&id=1NyyECSKU0jOwiUbVSvuGw9SIuLT8of6D";
+        VidPadel.src="https://drive.google.com/file/d/1RNi4ng6pxVvLT9-ZSbQvPE0lZO-qXp8V/view?usp=sharing";
+        VidTenis.src="https://drive.google.com/file/d/1SY0V5l7IQyKuOY03kn8CAn-nPBGY6liK/view?usp=sharing";
+        VidFutbol.src="https://drive.google.com/file/d/1NyyECSKU0jOwiUbVSvuGw9SIuLT8of6D/view?usp=sharing";
         // Los vídeos en segundo plano, se reproducen desde el principio.
         VidPadel.currentTime = 0;
         VidPadel.play();
